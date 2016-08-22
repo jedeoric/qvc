f95314010031ddabe8a0f5f73c6d19148058839a
jede <jede@oric.org>
2016-08-22 19:22:19 +0200

    some adds (almost finished)

--- src/qvc.asm
+++ src/qvc.asm
@@ -12,17 +12,43 @@
 
 #define RES $00
 #define RESB $02
-
+start
+.(
 	lda #$02
-	sta $026a
+	sta $026a ;FIXME
 	jmp start_qvc
+.)
+_a808
+.(
+	LDA #$B0
+	LDY #$8F
+	STA $04
+	STY $05
+	LDA #$FF
+	STA $03
+_a814
+	LDY #$4F
+_a816
+	LDA ($04),Y
+	CMP #$20
+	BNE _a82e
+	DEY
+	BPL _a816
+	LDA $04
+	SEC
+	SBC #$50
+	STA $04
+	BCS _a82a
+	DEC $05
+_a82a
+	DEC $03
+	BNE _a814
+_a82e
+	RTS
 
-.byt $A9,$B0,$A0,$8F,$85,$04,$84,$05
-.byt $A9,$FF,$85,$03,$A0,$4F,$B1,$04,$C9,$20,$D0,$12,$88,$10,$F7,$A5
 
-
-.byt $04,$38,$E9,$50,$85,$04,$B0,$02,$C6,$05,$C6,$03,$D0,$E6,$60,$00
-
+.byt $00
+.)
 _a830
 set_via_qvc
 .(
@@ -42,48 +68,84 @@ loop7
 	beq loop7
 	lda $030D
 	rts
-.)
-
 /**/
 .byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.)
 
 _a860
 .(
 	lda $f3
 	beq end
 	ldx #$11
-	jsr $a9c0 ; FIXME
+	jsr _a9c0 ; FIXME
 	dec $f3
 	lda $f9
 	beq end
 	dec $f9
 end	
 	rts
-.)
 .byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.)
 
 _a880
+.(
 	lda $f4
 .byt $F0,$23 ; FIXME
 	dec $f4
-	lda $f7
+	lda PTR1_QVC
+	sec
+	sbc #$50
+	sta PTR1_QVC
 	
-.byt $38,$E9,$50,$85,$F7,$B0,$02,$C6
-.byt $F8,$A5,$C1,$F0,$0D,$C6,$C1,$A5,$F5,$38,$E9,$28,$85,$F5,$B0,$02
-.byt $C6,$F6,$A2,$19,$20,$C0,$A9,$60,$00,$00,$00,$00,$00,$00,$00,$00
-.byt $A9,$00,$85,$F9,$A9,$00,$85,$F3,$A9,$30,$8D,$91,$BB,$8D,$90,$BB
-.byt $60,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+	bcs next16
+	dec $f8
+next16
+	lda $c1
+	beq next15
+	dec $c1
+	lda PTR2_QVC
+	sec
+	sbc #$28
+	sta PTR2_QVC
+	bcs next15
+	dec $f6
+next15
+	ldx #$19
+	jsr _a9c0
+	rts
+	.byt $00,$00,$00,$00,$00,$00,$00,$00
+.)	
+_a8b0
+.(
+	lda #$00
+	sta $f9
+	lda #$00
+	sta $f3
+	lda #$30
+	sta $bb91
+	sta $bb90
+	rts
+.byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.)
 
 _a8d0
+.(
+	CMP #$18
+	BNE _a8df
+	LDA $026A
+	EOR #$08
+	STA $026A
+	JMP _abf5
+_a8df
+	JMP _aac0 ; 
 
-.byt $C9,$18,$D0,$0B,$AD,$6A,$02,$49,$08,$8D,$6A,$02,$4C,$F5,$AB,$4C
-.byt $C0,$AA,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
-
+.byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.)
 _a8f0
 
 
 ROUTINE2_QVC
-
+.(
 	lda PTR1_QVC
 	ldy PTR1_QVC+1
 	sta RES
@@ -145,44 +207,108 @@ next5
 	;bne loop2
 .byt $D0,$DC ; FIXME
 	rts
+
 .byt $00,$00,$00,$00,$00,$00,$00,$00
 
-_a960
+
 
 .byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+
+.)
+
+_a960
+status_line_buffer_qvc
+.(
 .byt $2A,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$20,$58,$3A
 .byt $30,$30,$20,$20,$20,$59,$3A,$30,$30,$30,$20,$20,$20,$20,$20,$20
-.byt $20,$20,$20,$20,$20,$20,$20,$20,$00,$00,$00,$00,$00,$00,$00,$00
+.byt $20,$20,$20,$20,$20,$20,$20,$20
+// next is a garbage
+.byt $00,$00,$00,$00,$00,$00,$00,$00
+.)
+
 
 _a990
 display_status_line_qvc
+.(
 	ldx #$27
 loop
-	lda $a960,x ; FIXME
+	lda status_line_buffer_qvc,x ; FIXME
 	sta $bb80,x
 	dex 
 	bpl loop
 	rts
 .byt $00,$00,$00,$00
-.byt $BD,$80,$BB,$C9,$39,$F0,$07,$18,$69,$01,$9D,$80,$BB,$60,$A9,$30
-.byt $9D,$80,$BB,$CA,$D0,$EA,$60,$00,$00,$00,$00,$00,$00,$00,$00,$00
-.byt $BD,$80,$BB,$C9,$30,$F0,$07,$38,$E9,$01,$9D,$80,$BB,$60,$A9,$39
-.byt $9D,$80,$BB,$CA,$D0,$EA,$60,$00,$00,$00,$00,$00,$00,$00,$00,$00
-.byt $A5,$F3,$C9,$4F,$F0,$0F,$E6,$F3,$A5,$F9,$C9,$27,$F0,$02,$E6,$F9
-.byt $A2,$11,$4C,$A0,$A9,$AD,$80,$BB,$10,$06,$60,$00,$00,$00,$00,$00
+.)
 
-_aa00
+_a9a0
+.(
+	lda $bb80,x
+	cmp #$39
+	beq next15
+	clc
+	adc #$01
+	sta $bb80,x
+	rts
+next15
+	lda #$30
+	sta $bb80,x
+	dex
+.byt $D0,$EA
+	rts
 
-.byt $20,$B0,$A8,$4C,$10,$AA,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.byt $00,$00,$00,$00,$00,$00,$00,$00,$00
+.)
 
-_aa10
-	lda PTR3_QVC+1 ; FIXME
+_a9c0
+.(
+	lda $bb80,x
+	cmp #$30
+	beq next15
+	sec
+	sbc #$01
+	sta $bb80,x
+	rts
+next15
+	lda #$39
+	sta $bb80,x
+	dex
+	.byt $d0,$ea ; FIXME
+	rts
+.byt $00,$00,$00,$00,$00,$00,$00,$00,$00
+.)
+
+_a9e0
+.(
+	lda $f3
+	cmp #$4f
+	beq next16
+	inc $f3
+	lda $f9
+	cmp #$27
+	beq next15
+	inc $f9
+next15
+	ldx #$11
+	jmp _a9a0  ; FIXME
+next16
+	lda $bb80
+	bpl _aa00
+	rts
+.byt $00,$00,$00,$00,$00
+
++_aa00
+	jsr _a8b0 
+	jmp _aa10 
+.byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+
++_aa10
+	lda PTR3_QVC+1 ; 
 	cmp #$ff
 .byt $F0,$27 ; FIXME
-	lda $f7 ; FIXME
+	lda PTR1_QVC ; FIXME
 	clc
 	adc #$50
-	sta $f7; FIXME 
+	sta PTR1_QVC; FIXME 
 	lda $f8 ; FIXME
 	adc #$00
 	sta $f8
@@ -194,19 +320,24 @@ _aa10
 	adc #$28
 	sta PTR2_QVC
 
-.byt $90,$02
+.byt $90,$02 ; FIXME
 	inc $f6 ; FIXME
 	inc $c1
 	inc PTR3_QVC+1
 	ldx #$19
-	jsr $a9a0
+	jsr _a9a0 
 	rts
+	.byt $00,$00
+.)
 
-
-.byt $00,$00
-.byt $A4,$F3,$91,$F7,$4C,$E0,$A9,$00,$00,$00,$00,$00,$00,$00,$00,$00
+_aa40
+.(
+	ldy $f3
+	.byt $91,$F7 ; FIXME
+	jmp _a9e0
+.byt $00,$00,$00,$00,$00,$00,$00,$00,$00
 .byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
-
+.)
 
 _aa60
 .(
@@ -217,11 +348,10 @@ _aa60
 	rts
 .byt $00,$00,$00,$00,$00,$00,$00
 .)
+
 _aa70
-
-
-
 start_qvc
+.(
 	jsr init_buffer_text_qvc
 	lda #<QVC_BUFFER_TEXT
 	ldy #>QVC_BUFFER_TEXT
@@ -248,88 +378,104 @@ start_qvc
 	nop
 	nop
 	nop
+_aa95
 	jsr ROUTINE2_QVC
 	nop
 	nop
 	nop
+	
+#ifdef CPU_65C02
+	stz $02df
+#else
 	lda #$00
 	sta $02df
-
+#endif
 	jsr _aac0 
-	
-.byt $C9,$1B,$D0,$03,$EA,$EA,$60,$20,$40,$AA,$4C,$95,$AA
+	cmp #$1b
+	bne next15
+
+	nop
+	nop
+	rts
+next15
+	jsr _aa40 
+	jmp _aa95 
+
 .byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.)
 
 _aac0
+#ifdef CPU_65C02
+	stz $02df  ; FIXME
+#else
 	lda #$00
-	sta $02df
+	sta $02df  ; FIXME
+#endif
 	jsr _ada0 
 	cmp #$09
 	bne compare_next
-
-
-	jsr $a9e0 ; FIXME
+	jsr _a9e0 
 	jmp _abf5 
 compare_next
 	cmp #$0A
 	bne compare_next2
-	jsr $aa10 ; FIXME
+	jsr _aa10 
 	jmp _abf5 
 compare_next2
 	cmp #$0b
 	bne compare_next3
-	jsr $a880  ; FIXME
+	jsr _a880  
 	jmp _abf5  
 compare_next3
 	cmp #$08
 	bne compare_next4
-	jsr $a860  ; FIXME
+	jsr _a860  
 	jmp _abf5 
 compare_next4
 _aaf0
 	cmp #$0d
 	bne compare_next5
-	jsr $aa00 ; FIXME
+	jsr _aa00 
 	jmp _abf5 
 compare_next5
 	cmp #$04
 	bne compare_next6
-	jsr $ac00 ; FIXME
+	jsr _ac00 
 	jmp _abf5 
 compare_next6
 	cmp #$7f
 	bne compare_next7
-	jsr $ac70 ; FIXME
+	jsr _ac70 
 	jmp _abf5 
 compare_next7
 	cmp #$13
 	bne compare_next8
-	jsr $ac80 ; FIXME
+	jsr _ac80 
 	jmp _abf5 
 compare_next8
 	cmp #$1a
 	bne compare_next9
-	jsr $ac90 ; FIXME
+	jsr _ac90 
 	jmp _abf5 
 compare_next9
 	cmp #$01
 	bne compare_next10
-	jsr $ace0 ; FIXME
+	jsr _ace0 
 	jmp _abf5 
 compare_next10
 	cmp #$17
 	bne compare_next11
-	jsr $ad30 ; FIXME
+	jsr _ad30 
 	jmp _abf5 
 compare_next11
 	cmp #$11
 	bne compare_next12
-	jsr $ad50 ; FIXME
+	jsr _ad50 
 	jmp _abf5 
 compare_next12
 	cmp #$05
 	bne compare_next13
-	jsr $ad70 ; FIXME
+	jsr _ad70 
 	jmp _abf5 
 compare_next13
 	cmp #$10
@@ -337,7 +483,7 @@ compare_next13
 
 	pla
 	pla 
-	jmp $aa70 ; FIXME
+	jmp _aa70 
 compare_next14
 	cmp #$41
 	bmi next8
@@ -352,35 +498,40 @@ compare_next14
 	nop
 next8
 	cmp #$1b
-
 	bne next7
 	rts
 next7
 	cmp #$0f
 	bne compare_next15
-	jsr _ab80 ;FIXME
-	jmp _abf5 ;FIXME
+	jsr _ab80 
+	jmp _abf5 
 compare_next15
 	cmp #$20
 
 	bmi next9
 	rts
 next9
-	jmp $a8d0 ;FIXME
+	jmp _a8d0 
 _ab80
 
-	jsr $a808
+	jsr _a808
+#ifdef CPU_65C02
+	stz RESB
+	stz $02df ; FIXME
+	stz $04 ; FIXME
+#else	
 	lda #$00
 	sta RESB
 	sta $02df ; FIXME
 	sta $04 ; FIXME
+#endif
 	ldy #$40 ; BUFFER FIXME ?
 	sta RES
 	sty RES+1
 	
 	ldy #0
 	lda (RES),y
-	jsr set_via_qvc ; FIXME
+	jsr set_via_qvc 
 	iny
 	lda $02df
 	beq next10
@@ -440,68 +591,271 @@ store_char_buf
 	dex
 	bne store_char_buf
 	rts
-.)
-
-
-
 .byt $00,$00,$00
 _abf0
 .byt $00,$00,$00,$00,$00
+.)
 
 _abf5
+.(
 	jsr ROUTINE2_QVC
 	nop
 	nop
 	nop
 	jmp _aac0 
 .byt $00,$00
-
+.)
 _ac00
+.(
+	nop
+	nop
+	nop
+	ldy #$00
+loop18
+	lda (PTR1_QVC),y
+	cmp #$20
+	bne next21
 
-.byt $EA,$EA,$EA,$A0,$00,$B1,$F7,$C9,$20,$D0,$08,$C8,$C0,$50,$D0,$F5
-.byt $4C,$5D,$AC,$84,$00,$A0,$4F,$B1,$F7,$C9,$20,$D0,$03,$88,$10,$F7
-.byt $84,$02,$A9,$4F,$38,$E5,$02,$EA,$EA,$EA,$18,$65,$00,$4A,$85,$01
-.byt $A0,$4F,$B1,$F7,$99,$30,$BB,$88,$10,$F8,$A5,$00,$D0,$04,$A5,$02
+	iny
+	cpy #$50
+	bne loop18
 
-.byt $F0,$1B,$A0,$4F,$A9,$20,$91,$F7,$88,$10,$FB,$A6,$00,$A4,$01,$BD
-.byt $30,$BB,$91,$F7,$E4,$02,$F0,$05,$E8,$C8,$4C,$4F,$AC,$60,$00,$00
+	jmp end2 
+next21
+	sty RES
+	ldy #$4f
+loop19
+	lda (PTR1_QVC),y
+	cmp #$20
+	bne next20
+
+	dey
+	bpl loop19
+
+next20
+	sty RESB
+	lda #$4f
+	sec
+	sbc RESB
+	nop
+	nop
+	nop
+	clc
+	adc RES
+	lsr 
+	sta RES+1
+	ldy #$4f
+loop17
+	lda (PTR1_QVC),y
+	sta $bb30,y
+	dey
+	bpl loop17
+
+	lda RES
+	bne next16
+	lda RESB
+
+.byt $F0,$1B ; FIXME
+next16
+	ldy #$4f
+	lda #$20
+loop8
+	sta (PTR1_QVC),y
+	dey
+	bpl loop8
+
+	ldx RES
+
+	ldy RES+1
+_ac4f
+	lda $bb30,x
+	sta (PTR1_QVC),y
+	cpx RESB
+	beq end2
+
+	inx
+	iny
+	jmp _ac4f ; FIXME
+end2
+
+	rts
+
+.byt $00,$00
 .byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
-
+.)
 _ac70
-	jsr $a860 ; FIXME
+.(
+	jsr _a860 
 	ldy $f3
 	lda #$20
 .byt $91,$F7 ; FIXME
 	rts
 .byt $00,$00,$00,$00,$00,$00
+.)
 
 _ac80
+.(
+	ldy #$4f
+	lda #$20
+loop7
+	sta (PTR1_QVC),y
+	dey
+	bpl loop7
+	rts
+.byt $00,$00,$00,$00,$00,$00
+.)
 
-.byt $A0,$4F,$A9,$20,$91,$F7,$88,$10,$FB,$60,$00,$00,$00,$00,$00,$00
-.byt $A5,$F4,$C9,$FF,$F0,$3B,$A9,$60,$A0,$8F,$85,$00,$84,$01,$A9,$B0
-.byt $A0,$8F,$85,$02,$84,$03,$A0,$4F,$B1,$00,$91,$02,$88,$10,$F9,$A5
-.byt $00,$C5,$F7,$D0,$06,$A5,$01,$C5,$F8,$F0,$16,$A5,$00,$A4,$01,$85
-.byt $02,$84,$03,$A5,$00,$38,$E9,$50,$85,$00,$B0,$02,$C6,$01,$4C,$A6
-.byt $AC,$20,$80,$AC,$60,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
-.byt $A5,$F4,$C9,$FF,$F0,$44,$A5,$F7,$A4,$F8,$85,$00,$84,$01,$18,$69
-.byt $50,$85,$02,$98,$69,$00,$85,$03,$A0,$4F,$B1,$02,$91,$00,$88,$10
 
-_ad00
 
-.byt $F9,$A5,$02,$A4,$03,$85,$00,$84,$01,$A5,$02,$18,$69,$50,$85,$02
-.byt $90,$02,$E6,$03,$A5,$00,$C9,$B0,$D0,$DE,$A5,$01,$C9,$8F,$D0,$D8
-.byt $A0,$4F,$A9,$20,$99,$B0,$8F,$88,$10,$FA,$60,$00,$00,$00,$00,$00
-.byt $A4,$F3,$C0,$4F,$F0,$0D,$A0,$4F,$88,$B1,$F7,$C8,$91,$F7,$88,$C4
-.byt $F3,$D0,$F5,$A9,$20,$91,$F7,$60,$00,$00,$00,$00,$00,$00,$00,$00
-.byt $A4,$F3,$C0,$4F,$F0,$0F,$C8,$B1,$F7,$88,$91,$F7,$C8,$C0,$4F,$D0
-.byt $F5,$A9,$20,$91,$F7,$60,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+_ac90
+.(
+	lda $f4
+	cmp #$ff
+.byt $F0,$3B
+	lda #$60
+	ldy #$8f
+	sta RES
+	sty RES+1
+	lda #$b0
+	ldy #$8f
+	sta RESB
+	sty RESB+1
+_aca6
+	ldy #$4f
+loop9
+	lda (RES),y
+	sta (RESB),y
+	dey
+	bpl loop9
+
+	lda RES
+	cmp PTR1_QVC
+
+	bne next19
+	lda RES+1
+	cmp $f8
+.byt $F0,$16 ;FIXME
+next19
+	lda RES
+	ldy RES+1
+	sta RESB
+	sty RESB+1
+	lda RES
+	sec
+	sbc #$50
+	sta RES
+	bcs next18
+
+	dec RES+1
+next18
+	jmp _aca6 
+	jsr _ac80
+	rts
+
+.byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.)
+_ace0
+.(
+	LDA $F4
+	CMP #$FF
+	BEQ _ad2a
+	LDA PTR1_QVC
+	LDY $F8
+	STA $00
+	STY $01
+	CLC
+	ADC #$50
+	STA $02
+	TYA
+	ADC #$00
+	STA $03
+_acf8
+	LDY #$4F
+_acfa
+	LDA ($02),Y
+	STA ($00),Y
+	DEY
+	BPL _acfa
+	LDA $02
+	LDY $03
+	STA $00
+	STY $01
+	LDA $02
+	CLC
+	ADC #$50
+	STA $02
+	BCC _ad14
+	INC $03
+_ad14
+	LDA $00
+	CMP #$B0
+	BNE _acf8
+	LDA $01
+	CMP #$8F
+	BNE _acf8
+	LDY #$4F
+	LDA #$20
+_AD24
+	STA $8FB0,Y
+	DEY
+	BPL _AD24
+_ad2a
+	RTS
+.)
+
+.byt $00,$00,$00,$00,$00
+
+_ad30
+.(
+	LDY $F3
+	CPY #$4F
+	BEQ _ad43
+	LDY #$4F
+_ad38
+	DEY
+	LDA (PTR1_QVC),Y
+	INY
+	STA (PTR1_QVC),Y
+	DEY
+	CPY $F3
+	BNE _ad38
+_ad43
+	LDA #$20
+	STA (PTR1_QVC),Y
+	RTS
+	.byt $00,$00,$00,$00,$00,$00,$00,$00
+.)
+
+
+_ad50
+.(
+	LDY $F3
+	CPY #$4F
+	BEQ _ad65
+_ad56	
+	INY
+	LDA (PTR1_QVC),Y
+	DEY
+	STA (PTR1_QVC),Y
+	INY
+	CPY #$4F
+	BNE _ad56
+	LDA #$20
+	STA (PTR1_QVC),Y
+_ad65
+	RTS
+
+.byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.)
 
 _ad70
+.(
 	lda $bb80
 	eor #$80
 	sta $bb80
 	rts
 .byt $00,$00,$00,$00,$00
+.)
 _ad7e
 .(
 	lda $02df ; fixme
@@ -515,9 +869,10 @@ _ad7e
 	plp
 end
 	rts
+	.byt $00
 .)
 	
-.byt $00
+
 
 _ad90
 .(
@@ -536,17 +891,18 @@ next14
 	rts
 .)
 _ada0
-	jsr $aa60 ; FIXME
-	jsr _ad90 ; FIXME
+.(
+	jsr _aa60 
+	jsr _ad90
 	bmi next13
 	nop
-	jsr $aa60 ; FIXME
-	jsr _ad90 ; FIXME
+	jsr _aa60 
+	jsr _ad90 
 	bpl _ada0
 
 next13
 	and #$7f
 	rts
 .byt $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
-
+.)
 
